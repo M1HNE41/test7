02ad3302259cb9d6930efdb2fd7c30e2c7f9496b
PETCU MIHNEA <80165616+M1HNE41@users.noreply.github.com>
2024-02-21 22:29:23 +0200

    Add files via upload

---
+++ api/index.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>My Website</title>
+</head>
+<body>
+    <h1>Welcome to my website!</h1>
+    <a href="/redirect-to-youtube" class="button">Go to YouTube</a>
+</body>
+</html>
